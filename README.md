46e09e428009fc590b3872b0e3749c0a7192ed01
Angel D. Munoz <scythes.lol@gmail.com>
2021-09-20 23:41:43 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Node'less web development
+[Perla]: https://github.com/AngelMunoz/Perla
+
+A testing repo for [Perla] for pure HTML/CSS/JS projects
+
+## Getting Started
+```
+dotnet tool restore
+dotnet perla serve
+```
+visit [localhost:7331](http://localhost:7331/)
---
+++ src/index.js
@@ -0,0 +1,40 @@
+import { LitElement, html } from 'lit'
+import { registerAll } from 'fsharp-components'
+import styles from './styles.css' assert { type: 'css' };
+registerAll()
+class MyApp extends LitElement {
+    render() {
+        return html`
+        <article>
+            <header>
+                <button @click=${() => this.menuOpen = !this.menuOpen}>Menu</button>
+                <fs-off-canvas closable ?is-open=${this.menuOpen} @fs-close-off-canvas=${() => this.menuOpen = false}>
+                    <ul>
+                        <li>Yup, using npm dependencies</li>
+                        <li>Without Node and Npm!</li>
+                    </ul>
+                </fs-off-canvas>
+            </header>
+            <fs-message kind="danger" header="Look Ma' No complicated setup!">
+                <h3>Hello there!</h3>
+                <p>No local dependencies, all is on the cloud!</p>
+            </fs-message>
+        </article>
+        `
+    }
+
+    static get styles() {
+        return [styles]
+    }
+
+    static get properties() {
+        return { menuOpen: { state: true } }
+    }
+
+    constructor() {
+        super()
+        this.menuOpen = false
+    }
+}
+
+customElements.define("my-app", MyApp)
