57bb8b2346943f2db2b8b8923f6484350deba3ca
Angel D. Munoz <scythes.lol@gmail.com>
2021-10-02 23:03:14 -0500

    add fable + sutil sample

---
+++ fable-sutil/src/Main.fs
@@ -0,0 +1,13 @@
+﻿module Main
+
+
+open Sutil
+open Fable.Core.JsInterop
+
+let registerAll: unit -> unit = importMember "fsharp-components"
+importSideEffects "./lit.js"
+importSideEffects "./styles.css"
+
+registerAll ()
+// Start the app
+App.view () |> Program.mountElement "fable-app"
