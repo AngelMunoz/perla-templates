5e4b847f9b8f06568ab67492b466caeb34f5a8fa
Angel D. Munoz <scythes.lol@gmail.com>
2022-04-30 12:23:57 -0500

    update solid template

--- solid-js/src/index.js
+++ solid-js/src/index.js
@@ -2,40 +2,43 @@ import './index.css';
 //@ts-ignore
 import { createSignal, onCleanup } from 'solid-js';
 //@ts-ignore
-import { render } from "solid-js/web";
+import { render, For } from "solid-js/web";
 //@ts-ignore
 import html from "solid-js/html";
 //@ts-ignore
 import todos from './todos.json?module';
 
+/**
+ * 
+ * @param {{todo: Todo}} todo 
+ */
+function Todo({ todo }) {
+    return html`<li>${todo.title} - ${todo.done ? 'done' : 'pending'}</li>`;
+}
 
 /**
  * 
- * @param {Todo[]} items 
+ * @param {{ items: Todo[] }} items 
  */
-function todoList(items) {
-    /**
-     * 
-     * @param {Todo} item 
-     */
-    function todoItem(item) {
-        return html`<li>${item.title} - ${item.done ? 'done' : 'pending'}</li>`;
-    }
+function TodoList({ items }) {
+    const fallback = html`<li>Loading...</li>`;
+    const content = (/** @type {Todo} */ todo) => html`<${Todo} todo=${todo} />`;
     return html`
         <ul>
-            ${items.map(todoItem)}
+            <${For} each=${items} fallback=${fallback}>${content}<//>
         </ul>
     `;
 }
 
-
 const App = () => {
-    const [count, setCount] = createSignal(0),
-        timer = setInterval(() => setCount(count() + 1), 1000);
+    const [count, setCount] = createSignal(0);
+    const timer = setInterval(() => setCount(count() + 1), 1000);
+
     onCleanup(() => clearInterval(timer));
+
     return html`
         <h1>Hello <b>solid.js!</b> - ${count}</h1>
-        ${todoList(todos)}
+        <${TodoList} items=${todos} />
     `;
 };
-render(App, document.querySelector("#app"));
+render(() => html`<${App} />`, document.querySelector("#app"));
