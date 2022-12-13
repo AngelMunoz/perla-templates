5e0db38463078810d2de30282fdc1e21218c33bd
Angel D. Munoz <scythes.lol@gmail.com>
2022-12-13 14:57:30 -0600

    update lit-js sample

--- lit-js/src/index.js
+++ lit-js/src/index.js
@@ -1,67 +1,65 @@
-import './index.css';
+import "./index.css";
 //@ts-ignore
-import { LitElement, html } from 'lit';
+import { LitElement, html } from "lit";
 //@ts-ignore
-import todos from './todos.json?module';
+import todos from "./todos.json?module";
 
 /**
  * Renders a simple todo on the screen
- * @param {Todo} todo 
+ * @param {Todo} todo
  */
-const todoTpl = todo => html`<li>${todo.title} - ${todo.done ? "Done" : "Pending"}</li>`;
-
+const todoTpl = (todo) =>
+  html`<li>${todo.title} - ${todo.done ? "Done" : "Pending"}</li>`;
 
 class MyApp extends LitElement {
+  constructor() {
+    super();
+    this.mouseCtrl = new MouseController(this);
+  }
 
-    constructor() {
-        super();
-        this.mouseCtrl = new MouseController(this);
-    }
-
-    render() {
-        return html`
-        <article>
-            <h1>Hello, World!</h1>
-            <p>Your mouse is at: (${this.mouseCtrl.x}, ${this.mouseCtrl.y})</p>
-            <ul>
-                ${todos.map(todoTpl)}
-            </ul>
-        </article>
-        `;
-    }
+  render() {
+    return html`
+      <article>
+        <h1>Hello, World!</h1>
+        <p>Your mouse is at: (${this.mouseCtrl.x}, ${this.mouseCtrl.y})</p>
+        <ul>
+          ${todos.map(todoTpl)}
+        </ul>
+      </article>
+    `;
+  }
 }
 
 export class MouseController {
+  /**
+   * @param {LitElement} host
+   */
+  constructor(host) {
+    this.host = host;
+    host.addController(this);
+    this.x = 0;
+    this.y = 0;
+  }
 
-    constructor(host) {
-        this.host = host;
-        host.addController(this);
-        this.x = 0;
-        this.y = 0;
-    }
+  _updatePosition =
+    /**
+     *
+     * @param {MouseEvent} event
+     */
+    (event) => {
+      this.x = event.x;
+      this.y = event.y;
+      this.host.requestUpdate();
+    };
 
-    _updatePosition =
-        /**
-         *
-         * @param {MouseEvent} event
-         */
-        (event) => {
-            this.x = event.x;
-            this.y = event.y;
-            this.host.requestUpdate();
-        };
+  hostConnected() {
+    window.addEventListener("mousemove", this._updatePosition);
+  }
 
-    hostConnected() {
-        window.addEventListener('mousemove', this._updatePosition);
-    }
-
-    hostDisconnected() {
-        window.removeEventListener('mousemove', this._updatePosition);
-    }
-
+  hostDisconnected() {
+    window.removeEventListener("mousemove", this._updatePosition);
+  }
 }
-
-
 
 //@ts-ignore
 customElements.define("my-app", MyApp);
