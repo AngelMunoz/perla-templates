e1361cb0a0c705bb2ae2d508e3ef69968fa06732
Angel D. Munoz <scythes.lol@gmail.com>
2022-12-14 08:35:04 -0600

    update react-jsx

--- react-jsx/src/main.jsx
+++ react-jsx/src/main.jsx
@@ -1,10 +1,13 @@
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App.jsx'
+import "./index.css";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App.js";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
