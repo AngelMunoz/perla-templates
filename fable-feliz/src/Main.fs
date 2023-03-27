932c4a4785f45d90705422672cbb738670aa343c
Angel D. Munoz <scythes.lol@gmail.com>
2023-03-27 16:52:43 -0600

    update Feliz template

--- fable-feliz/src/Main.fs
+++ fable-feliz/src/Main.fs
@@ -5,7 +5,5 @@ open App
 open Browser.Dom
 open Fable.Core.JsInterop
 
-ReactDOM.render(
-    Components.Router(),
-    document.getElementById "feliz-app"
-)
+let root = ReactDOM.createRoot(document.getElementById "feliz-app")
+root.render(Components.Counter())
